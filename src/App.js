8eb99468947496ee42a85b4dc11a812a3406258f
Rocky <yangrocky2013@gmail.com>
2021-04-12 19:04:33 -0400

    imported provided JSON data to main App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [companyData, setCompanyData] = useState(require('./assets/data/Companies.json'))
+  const [guestData, setGuestData] = useState(require('./assets/data/Guests.json'))
+  
   return (
     <h1>Hello world!</h1>
   );
